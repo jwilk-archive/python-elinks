9486da449900ef040db48da7aa2b392221ad9c17
Jakub Wilk <jwilk@jwilk.net>
2007-12-23 15:36:42 +0000

    setup.py: more metadata.

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
+'''
+python-elinks installs an encoding error handler that uses the same ASCII replacements as ELinks does.
+'''
+
+classifiers = '''\
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+License :: OSI Approved :: GNU General Public License (GPL)
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Text Processing :: Filters'''.split('\n')
+
 from distutils.core import setup
 
 setup(
-	name = 'elinks',
-	version = '0.1',
+	name = 'python-elinks',
+	version = '0.1.2',
+	license = 'GNU GPL 2',
+	platforms = ['any'],
+	description = 'ELinks-like encoding error handler',
+	long_description = __doc__.strip(),
+	classifiers = classifiers,
 	url = 'http://code.google.com/p/python-elinks/',
 	author = 'Jakub Wilk',
 	author_email = 'ubanus@users.sf.net',
