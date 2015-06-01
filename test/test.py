b88c26218a4037fbc69dc7af299546ab4c573757
Jakub Wilk <jwilk@jwilk.net>
2015-06-01 15:36:01 +0200

    test: trim whitespace.

--- test/test.py
+++ test/test.py
@@ -22,7 +22,7 @@ def test_elinks():
     s = u('Różowy słoń nie zechce usiąść na tępych gwoździach…')
     t = b('Rozowy sl/on nie zechce usiasc na tepych gwozdziach...')
     encoded_s = s.encode('ASCII', 'elinks')
-    assert encoded_s == t , '%r != %r' % (encoded_s, t)
+    assert encoded_s == t, '%r != %r' % (encoded_s, t)
 
 if __name__ == '__main__':
     test_elinks()
