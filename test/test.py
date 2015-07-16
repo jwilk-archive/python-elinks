4a2c1bd5521d0ad1260ff1f79d0c3d86956cb432
Jakub Wilk <jwilk@jwilk.net>
2015-07-16 12:07:00 +0200

    test: add copyright information.

--- test/test.py
+++ test/test.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 # encoding=UTF-8
 
+# Copyright © 2007-2015 Jakub Wilk <jwilk@jwilk.net>
+#
+# This package is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 dated June, 1991.
+
 import elinks
 
 try:
