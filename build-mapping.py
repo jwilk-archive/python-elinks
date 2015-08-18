c50bf23e9682a4fce7aa7f2862ab1be48f1411be
Jakub Wilk <jwilk@jwilk.net>
2015-08-18 13:16:23 +0200

    Update license declarations.

--- build-mapping.py
+++ build-mapping.py
@@ -64,10 +64,17 @@ def update(code, replace):
 
 print('''# encoding=UTF-8
 
-# This file was automatically generated from the Unicode/7bitrepl.lnx file of
-# ELinks 0.12 distribution. ELinks is free software; you can redistribute
-# it and/or modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; version 2 dated June, 1991.
+# This file was automatically generated from the Unicode/7bitrepl.lnx file
+# of ELinks 0.12 distribution.
+#
+# Elinks is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by the
+# Free Software Foundation.
+#
+# Elinks is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
 ''')
 for line in urlopen('http://repo.or.cz/w/elinks.git?a=blob_plain;f=Unicode/7bitrepl.lnx;hb=elinks-0.12'):
     line = line.strip()
